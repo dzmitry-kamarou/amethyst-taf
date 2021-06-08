41b463ea2e3f9fde9fbc6c215760023fda6b6ba1
dzmitry-kamarou <dzmitry.kamarou@outlook.com>
2021-06-08 21:40:13 +0300

    TA-17: add logging to UI (#6)
    Co-authored-by: Dzmitry Kamarou <dzmitry.kamarou@deltatre.it>

--- src/main/java/ui/page/DashboardPage.java
+++ src/main/java/ui/page/DashboardPage.java
@@ -3,16 +3,18 @@ package ui.page;
 import org.openqa.selenium.By;
 import ui.Browser;
 
-public class DashboardPage {
+public class DashboardPage extends BasePage {
 
   private final String logoutLinkXpath = "//a[@id='logout']";
   private final String greetingXpath = "//p[starts-with(text(),'Welcome ')]";
 
   public void clickLogOutLink() {
+    logger.debug("Click 'logout' link");
     Browser.getWebDriver().findElement(By.xpath(logoutLinkXpath)).click();
   }
 
   public String greetingText() {
+    logger.debug("Grab 'greeting' text");
     return Browser.getWebDriver().findElement(By.xpath(greetingXpath)).getText();
   }
 }
--- src/main/java/ui/page/LoginPage.java
+++ src/main/java/ui/page/LoginPage.java
@@ -3,7 +3,7 @@ package ui.page;
 import org.openqa.selenium.By;
 import ui.Browser;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
 
   private String titleHeadingXpath = "//h1[@id='title']";
   private String messageLabelXpath = "//span[@id='message']";
@@ -12,24 +12,29 @@ public class LoginPage {
   private String loginButtonXpath = "//button[@id='submit']";
 
   public LoginPage fillEmailField(String email) {
+    logger.debug(String.format("Enter '%s' to 'email' field", email));
     Browser.getWebDriver().findElement(By.xpath(emailFieldXpath)).sendKeys(email);
     return this;
   }
 
   public LoginPage fillPasswordField(String password) {
+    logger.debug(String.format("Enter '%s' to 'password' field", password));
     Browser.getWebDriver().findElement(By.xpath(passwordFieldXpath)).sendKeys(password);
     return this;
   }
 
   public void clickLoginButton() {
+    logger.debug("Click 'Login' button");
     Browser.getWebDriver().findElement(By.xpath(loginButtonXpath)).click();
   }
 
   public String messageLabelText() {
+    logger.debug("Get 'message' text");
     return Browser.getWebDriver().findElement(By.xpath(messageLabelXpath)).getText();
   }
 
   public String titleHeadingText() {
+    logger.debug("Grab 'Heading' text");
     return Browser.getWebDriver().findElement(By.xpath(titleHeadingXpath)).getText();
   }
 }
